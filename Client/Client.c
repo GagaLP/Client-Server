d6d1bbda55651ada4cfe9302730839d7f8eb1be3
GagaLP <GabrielMitterrutzner@gmail.com>
2017-04-12 20:33:31 +0200

    New

--- Client/Client.c
+++ Client/Client.c
@@ -2,3 +2,78 @@
 // Created by Gabriel Mitterrutzner on 12.04.17.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+//thread
+#include <pthread.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+
+
+void* threadOperation(int *arg){
+    int socket = *arg;
+    int serverMessageLength;
+    char serverMessage[1024];
+
+    while ((serverMessageLength = recv(socket , serverMessage , 2000 , 0)) > 0) {
+        if (serverMessage != "q") {
+            printf("%s\n", serverMessage);
+        }
+    }
+
+    printf("Client hat sich abgemeldet\n");
+
+    free(arg);
+    pthread_exit(NULL);
+}
+
+int main(int argc, const char * argv[]) {
+    pthread_t thread;
+    struct sockaddr_in serverAdress;
+    serverAdress.sin_family = AF_INET;
+    serverAdress.sin_port = htons(5000);
+    serverAdress.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd == -1) {
+        perror("socket()");
+        return 1;
+    }
+
+    printf("Warten auf Server\n");
+
+    if (connect(sockfd, (const struct sockaddr *) &serverAdress, sizeof(serverAdress)) == -1) {
+        perror("connect()");
+        return 2;
+    }
+
+    pthread_create(&thread, NULL, (void*(*)(void*)) &threadOperation, sockfd);
+
+    char test[1024];
+
+    do{
+        printf("Sende den Text: ");
+        scanf("%s",test);
+        if (send(sockfd, test, sizeof(test), 0) < 0) {
+            perror("send()");
+        }
+    }while (strcmp(test, "q"));
+
+    pthread_join(thread, NULL);
+
+    close(sockfd);
+
+    return 0;
+}
--- Server/Server.c
+++ Server/Server.c
@@ -2,3 +2,92 @@
 // Created by Gabriel Mitterrutzner on 12.04.17.
 //
 
+//normal
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+//
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+
+//thread
+#include <pthread.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+
+#define clear() printf("\033[H\033[J");
+
+void* threadOperation(int *arg){
+    int socket = *arg;
+    long clientMessageLength;
+    char clientMessage[1024];
+
+    while ((clientMessageLength = recv(socket, clientMessage , 2000 , 0)) > 0) {
+        if (strcmp(clientMessage, "q")) {
+            printf("%s\n", clientMessage);
+        }
+    }
+
+    printf("Client hat sich abgemeldet\n");
+
+    free(arg);
+    pthread_exit(NULL);
+}
+
+int main(int argc, const char * argv[]) {
+    int i = 0;
+    struct sockaddr_in my_addr;
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port = htons(5000);
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    int clientSokets = 0;
+
+    pthread_t threads[2];
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd == -1) {
+        perror("socket()");
+    }
+
+    if (bind(sockfd, &my_addr, sizeof(my_addr)) == -1) {
+        perror("bind()");
+    }
+
+    printf("Warte auf Clients\n");
+    listen(sockfd, 2);
+
+    socklen_t sin_size = sizeof(struct sockaddr_in);
+    struct sockaddr_in remote_host;
+
+    while (i < 2) {
+        clientSokets = accept(sockfd, &remote_host, &sin_size);
+        int* new_sock = malloc(1);
+        *new_sock = clientSokets;
+
+        if (clientSokets == -1) {
+            perror("accept()");
+        }
+
+        printf("Client %d ist da\n", i);
+
+        pthread_create(&threads[i], NULL, (void*(*)(void*)) &threadOperation, new_sock);
+        i++;
+    }
+
+    pthread_join(threads[0], NULL);
+    pthread_join(threads[1], NULL);
+    close(clientSokets);
+    close(sockfd);
+    printf("Current time: %s",__TIME__);
+    return 0;
+}
